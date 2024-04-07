9ba1117bd43ae97472750f0776dff73002375b3a
Alexander Vorobyov <aovorobyov@mail.ru>
2024-04-07 14:40:28 +0300

    main: Added new array of words

--- src/js/consts.js
+++ src/js/consts.js
@@ -1,3 +1,3 @@
-export const WORDS = ['code', 'frontend', 'programming'];
+export const WORDS = ['dune', 'instasamka', 'spring', 'manu', 'manuel', 'egypt', 'gym', 'habibi', 'mcdonalds', 'cat', 'spirit', 'mizulina', 'sber', 'tiktok', 'legday'];
 
 export const KEYBOARD_LETTERS = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
